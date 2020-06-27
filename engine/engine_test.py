ba43c562b70747e5c691d8cd7c6d29dd7bdcecbe
JakeTheCorn <jakethecorn@gmail.com>
2020-06-27 11:15:12 -0400

    add new failing case

--- engine/engine_test.py
+++ engine/engine_test.py
@@ -9,109 +9,110 @@ from engine import engine
 #   Test for Pluck errors (then implement)
 
 class EngineTest(unittest.TestCase):
-    def test_basic_flat(self):
-        tables = [
-            ({'name': 'bob'}, {'name': 'name'}, {'Name': 'name'}, {'Name': 'bob'}),
-            ({'age': 45}, {'age': 'age'}, {'Age': 'age'}, {'Age': 45}),
-            ({'age': 46}, {'hasAge': 'age'}, {'HasAge': 'hasAge'}, {'HasAge': 46})
-        ]
-        for data, in_doc, out_doc, expected in tables:
-            doc = {
-                'in': in_doc,
-                'out': out_doc
-            }
-            actual = engine(doc=doc, data=data)
-            self.assertEqual(actual, expected)
+  def test_basic_flat(self):
+    tables = [
+      ({'name': 'bob'}, {'name': 'name'}, {'Name': 'name'}, {'Name': 'bob'}),
+      ({'age': 45}, {'age': 'age'}, {'Age': 'age'}, {'Age': 45}),
+      ({'age': 46}, {'hasAge': 'age'}, {'HasAge': 'hasAge'}, {'HasAge': 46})
+    ]
+    for data, in_doc, out_doc, expected in tables:
+      doc = {
+          'in': in_doc,
+          'out': out_doc
+      }
+      actual = engine(doc=doc, data=data)
+      self.assertEqual(actual, expected)
 
-    def test_nested_to_flat(self):
-        data = {
-            'user' : {
-                'name' : 'Frank'
-            }
+  def test_nested_to_flat(self):
+    data = {
+      'user' : {
+        'name' : 'Frank'
+      }
+    }
+    doc = {
+      'in': {
+        'name' : 'user.name'
+      },
+      'out' : {
+        'Name' : 'name'
+      }
+    }
+
+    actual = engine(doc=doc, data=data)
+    expected = {'Name': 'Frank'}
+    self.assertEqual(actual, expected)
+
+  def test_flat_to_nested(self):
+    data = {
+      'user_first': 'bob',
+      'user_last': 'bobson'
+    }
+    doc = {
+      'in': {
+        'in_first' : 'user_first',
+        'in_last': 'user_last'
+      },
+      'out' : {
+        'user' : {
+          'first': 'in_first',
+          'last': 'in_last'
         }
-        doc = {
-            'in': {
-                'name' : 'user.name'
-            },
-            'out' : {
-                'Name' : 'name'
-            }
-        }
+      }
+    }
 
-        actual = engine(doc=doc, data=data)
-        expected = {'Name': 'Frank'}
-        self.assertEqual(actual, expected)
+    actual = engine(doc=doc, data=data)
+    expected = {'user': {'first': 'bob', 'last': 'bobson'}}
+    self.assertEqual(expected, actual)
 
-    def test_flat_to_nested(self):
-        data = {
-            'user_first': 'bob',
-            'user_last': 'bobson'
-        }
-        doc = {
-            'in': {
-                'in_first' : 'user_first',
-                'in_last': 'user_last'
-            },
-            'out' : {
-                'user' : {
-                    'first': 'in_first',
-                    'last': 'in_last'
-                }
-            }
-        }
+  def test_flat_to_deeply_nested(self):
+      data = {
+          'user_first': 'bob',
+          'user_last': 'bobson'
+      }
+      doc = {
+          'in': {
+              'in_first' : 'user_first',
+              'in_last': 'user_last'
+          },
+          'out' : {
+              'user' : {
+                  'name': {
+                      'first': 'in_first',
+                      'last': 'in_last'
+                  }
+              }
+          }
+      }
 
-        actual = engine(doc=doc, data=data)
-        expected = {'user': {'first': 'bob', 'last': 'bobson'}}
-        self.assertEqual(expected, actual)
+      actual = engine(doc=doc, data=data)
+      expected = {'user': {'name': {'first': 'bob', 'last': 'bobson'}}}
+      self.assertEqual(actual, expected)
 
-    def test_flat_to_deeply_nested(self):
-        data = {
-            'user_first': 'bob',
-            'user_last': 'bobson'
-        }
-        doc = {
-            'in': {
-                'in_first' : 'user_first',
-                'in_last': 'user_last'
-            },
-            'out' : {
-                'user' : {
-                    'name': {
-                        'first': 'in_first',
-                        'last': 'in_last'
-                    }
-                }
-            }
-        }
+  def test_it_errors_when_no_in_or_out_keys_in_doc(self):
+      doc = {'in': None}
+      _res, err = engine(doc, {})
+      self.assertEqual('Doc should contain "in" and "out" keys', err)
+      doc = {'out': None}
+      _res, err = engine(doc, {})
+      self.assertEqual('Doc should contain "in" and "out" keys', err)
 
-        actual = engine(doc=doc, data=data)
-        expected = {'user': {'name': {'first': 'bob', 'last': 'bobson'}}}
-        self.assertEqual(actual, expected)
+  def test_in_datatype_err(self):
+      doc = {'in': None, 'out': {}}
+      _res, err = engine(doc, {})
+      self.assertEqual('Doc "in" should be of type dict', err)
 
-    def test_it_errors_when_no_in_or_out_keys_in_doc(self):
-        doc = {'in': None}
-        _res, err = engine(doc, {})
-        self.assertEqual('Doc should contain "in" and "out" keys', err)
-        doc = {'out': None}
-        _res, err = engine(doc, {})
-        self.assertEqual('Doc should contain "in" and "out" keys', err)
+  def test_out_datatype_err(self):
+    doc = {'in': {}, 'out': None}
+    _res, err = engine(doc, {})
+    self.assertEqual('Doc "out" should be of type dict', err)
 
-    def test_in_datatype_err(self):
-        doc = {'in': None, 'out': {}}
-        _res, err = engine(doc, {})
-        self.assertEqual('Doc "in" should be of type dict', err)
-
-    def test_out_datatype_err(self):
-        doc = {'in': {}, 'out': None}
-        _res, err = engine(doc, {})
-        self.assertEqual('Doc "out" should be of type dict', err)
-
-    def test_bad_path_error(self):
-        doc = {
-            'in': {},
-            'out': None
-        }
-        _res, err = engine(doc, {})
+  # todo
+  def _test_list_data(self):
+    data = []
+    doc = {'in': {}, 'out': {}}
+    actual, err = engine(doc, data)
+    expected = {}
+    self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
